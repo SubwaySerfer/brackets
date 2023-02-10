4bfb18b1878032c5ccae9e09e4073ec5c98851e7
Sasha Afanasyev <afonia.marcii@gmail.com>
2023-02-10 16:51:40 +0300

    add my code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let arr = str.split('');
+  let stack = [];
+
+  // проходит по каждому символу
+  for (let i = 0; i < arr.length; i++) {
+    let currentSymbol = arr[i];
+
+    let topElement = stack[stack.length - 1];
+    // Проходит по каждому массиву конфигурации
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      // Поиск нужной конфигурации скобок
+      if (bracketsConfig[j].includes(currentSymbol)) {
+        // Если 0 и 1 элемент массива не одинаковый
+        if (bracketsConfig[j][0] !== bracketsConfig[j][1]) {
+          if (currentSymbol === bracketsConfig[j][0]) {
+            stack.push(currentSymbol);
+            // break;
+          } else if (currentSymbol === bracketsConfig[j][1]) {
+            if (topElement === bracketsConfig[j][0]) {
+              stack.pop();
+            } else if (stack.length === 0) {
+              return false;
+            } else {
+              return false;
+            }
+          }
+        } else {
+          // Если символы в паре одинаковые, то действует эта связка
+          if (!stack.includes(bracketsConfig[j][0])) {
+            stack.push(currentSymbol);
+          } else if (topElement == bracketsConfig[j][0]) {
+            stack.pop();
+          } else {
+            return false;
+          }
+        }
+      }
+    }
+  }
+  return stack.length === 0;
+};
